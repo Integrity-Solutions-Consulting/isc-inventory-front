3219298d2333dc43345ee786249442d1bb7dc4f3
ricsiav <adrian2x2001@gmail.com>
2025-06-13 17:21:00 -0400

    correcion de la url de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://isc-inventory-back.onrender.com/api/v1/',
+  apiBaseUrl: 'https://isc-inventory-back.onrender.com/api/v1',
   adminBaseUrl: 'http://localhost:8081/admin/',
 };
