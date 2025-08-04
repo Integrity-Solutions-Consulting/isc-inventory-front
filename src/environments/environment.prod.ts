ae214772de32fbcd856e45f0af6ca82e05bdec14
ricsiav <adrian2x2001@gmail.com>
2025-08-04 11:37:55 -0500

    configuracion para despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authBaseUrl: 'https://isc-inventory-back.onrender.com/api/v1',
-  apiBaseUrl: 'https://isc-inventory-back-api.onrender.com/api/v1'
+  authBaseUrl: 'http://147.93.181.147:8080/api/v1',
+  apiBaseUrl: 'http://147.93.181.147:8081/api/v1'
 };
