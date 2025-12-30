02e1fe2783e69def6d2a4054eb3a24acaad07cc9
Xavier Tirado <xavier.tirado@integritysolutions.com.ec>
2025-12-30 15:27:13 -0500

    Cambio de link en docker-compose y enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  authBaseUrl: 'https://auth.inv.dokploy.integritysolutions.com.ec',
-  apiBaseUrl: 'https://api.inventory.integritysolutions.com.ec'
+  authBaseUrl: 'https://auth.inv.dokploy.integritysolutions.com.ec/api/v1',
+  apiBaseUrl: 'https://api.inv.dokploy.integritysolutions.com.ec/api/v1'
 };
