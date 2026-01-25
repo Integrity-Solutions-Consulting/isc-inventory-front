010b29bc8449043b30dd8255b20227e83e2746cc
Jesús Guzmán <55502763+yigsvnsla@users.noreply.github.com>
2026-01-25 11:53:28 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  authBaseUrl: 'https://dev.auth.inv.dokploy.integritysolutions.com.ec/api/v1',
-  apiBaseUrl: 'https://dev.api.inv.dokploy.integritysolutions.com.ec/api/v1'
+  authBaseUrl: 'https://dev.auth.dokploy.integritysolutions.com.ec/api/v1',
+  apiBaseUrl: 'https://dev.api.dokploy.integritysolutions.com.ec/api/v1'
 };
